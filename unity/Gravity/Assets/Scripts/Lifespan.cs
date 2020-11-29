ae5bca151c007edc63d5b9bd906fc5ac4b69f658
Carl Dietz <s3668498@student.rmit.edu.au>
2020-11-29 15:42:21 +1100

    Added rocket trail

---
+++ unity/Gravity/Assets/Scripts/Lifespan.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lifespan : MonoBehaviour
+{
+    public int lifeTime;
+
+    public GameObject target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lifeTime < 1)
+        {
+            GameObject.Destroy(target);
+        }
+        lifeTime--;
+    }
+}
