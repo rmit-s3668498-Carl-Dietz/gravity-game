042e0c2f5c417bdecf33cbf6a9f12990c0c8b594
Carl Dietz <s3668498@student.rmit.edu.au>
2020-12-01 15:41:28 +1100

    updated rocket trail

--- unity/Gravity/Assets/Scripts/RocketTrail.cs
+++ unity/Gravity/Assets/Scripts/RocketTrail.cs
@@ -14,9 +14,14 @@ public class RocketTrail : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xPos = rigidBody.position.x+Mathf.Cos(Mathf.Deg2Rad* (rigidBody.rotation-90))/5;
+        float yPos = rigidBody.position.y+Mathf.Sin(Mathf.Deg2Rad* (rigidBody.rotation-90))/5;
+
+        print(rigidBody.rotation);
+
         if(keyHeld && (Time.frameCount%7 == 0))
         {
-            Instantiate(trail, new Vector2(rigidBody.position.x, rigidBody.position.y), Quaternion.identity);
+            Instantiate(trail, new Vector2(xPos, yPos), Quaternion.identity);
         }
         
         if(Input.GetKeyDown(KeyCode.UpArrow))
